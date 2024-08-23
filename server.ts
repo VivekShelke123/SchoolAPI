ca3f23bc9d86f025a6ed5d6912449bb9e0fd045a
Vivek <viveklearning2003@gmail.com>
2024-08-23 15:58:41 +0530

    some changes

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port , ()=>{
     console.log(`Server on Port=${port} Started Successfully`);
